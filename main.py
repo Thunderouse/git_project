68607c81aed32b73ae06504d2475449fee8a906b
Yaroslav Reutov <yarrr9@gmail.com>
2022-11-05 13:31:11 +0300

    add superfunction

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+def my_superfunction():
+    print("Больше принтов богу принтов")
+
 def main():
     print("Принт 1")
     print("Принт 2")
     print("ПЕНТАКИЛ СИЛА, KDA  МОГИЛА")
     print("И еще один принт")
+    my_superfunction()
 
 if __name__ == "__main__":
     main()
